1074aa84faa6b57527045586675f79bb5d81ea4b
Nicholas Crossley <ncwebworks@gmail.com>
2021-11-05 21:26:49 -0400

    finished ui

---
+++ app.js
@@ -0,0 +1,11 @@
+let memory = 0;
+let input = null;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+	onClick(button.id);
+});
+
+function opHandler(operator) {}
+function numHandler(number) {}
+function memHandler(button) {}
